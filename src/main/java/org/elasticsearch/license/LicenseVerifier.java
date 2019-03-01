db5291823d92e74778d4704966f3de983e46a979
bobo <yabo_han@163.com>
2019-03-01 14:48:39 +0800

    first commit

---
+++ src/main/java/org/elasticsearch/license/LicenseVerifier.java
@@ -0,0 +1,18 @@
+package org.elasticsearch.license;
+
+/**
+ * @author bobo
+ * @Description:
+ * @date 2019-03-01 13:09
+ */
+public class LicenseVerifier {
+
+    public static boolean verifyLicense(final License license, final byte[] encryptedPublicKeyData) {
+        return true;
+    }
+
+    public static boolean verifyLicense(final License license) {
+        return true;
+    }
+
+}
